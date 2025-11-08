db7454c2815a87525b1a50f1d46f69cf6bcc4bfe
Pathum-Piyumal <22cse0388@ms.sab.ac.lk>
2025-11-08 21:35:19 +0530

    create routes:route.js

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+const tasks = require('./route/tasks');
+
+//middleware
+app.use(express.json());
+
+
+
 
 //routes
-
 app.get('/hello', (req, res) => {
     res.send('Task Manager')
 })
 
+
+
+app.use('/api/v1/tasks', tasks);
+ 
 //app.get('/api/v1/tasks')   -get all the tasks
 //app.post('/api/v1/tasks')  -create a new task
 //app.get('/api/v1/tasks/:id') -get single task
